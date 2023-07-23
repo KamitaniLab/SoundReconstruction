44096993f0b95bb50f7d08dcac8dd74ec7dfc4c2
micchu <bte0073misato@gmail.com>
2023-07-23 15:23:13 +0900

    [add] add 2_test_batch.sh

---
+++ 2_test_batch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd feature_decoding
+python featdec_fastl2lir_predict.py config/vggsound_fmriprep_rep4_500voxel_vggishish_allunits_fastl2lir_alpha100.yaml
+python featdec_fastl2lir_predict.py config/vggsound_attention_fmriprep_rep4_500voxel_vggishish_allunits_fastl2lir_alpha100.yaml
